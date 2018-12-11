33699dce32e94cd3b22ef0d86eacdddd730dcd6e
3pls0de <43237480+3pls0de@users.noreply.github.com>
2018-12-12 06:46:42 +0700

    Add files via upload

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $config = [
-"imei" => "621750464413334",
-"sign" => "3dc057a327530831fe982f361d5f8ff09bff5462",
-"token" => "d4ec2c2f54dbb64aa82ad44e38991daa",
-"uuid" => "d7b77b0c-1045-47f7-ace7-58f4b4d6857d"
+"imei" => "628562169246239",
+"sign" => "b43ecc00b68b15dc5fe9623fb84289ee28b0d557",
+"token" => "4f0e91499ca08c6fa56db0339fa1403c",
+"uuid" => "932fe778-08be-480f-81b5-d43116529613"
 ];
