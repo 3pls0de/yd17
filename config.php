610b3bf6d6564c86f4c0f0f205890f5cc1399341
3pls0de <43237480+3pls0de@users.noreply.github.com>
2018-12-12 06:41:45 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$config = [
+"imei" => "621750464413334",
+"sign" => "3dc057a327530831fe982f361d5f8ff09bff5462",
+"token" => "d4ec2c2f54dbb64aa82ad44e38991daa",
+"uuid" => "d7b77b0c-1045-47f7-ace7-58f4b4d6857d"
+];
